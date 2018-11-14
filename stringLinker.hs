51d1bc96585eaef5dfa663ba82577844420a5b39
Jonathan Tanner <nixCodeX@users.noreply.github.com>
2018-11-14 14:22:42 +0000

    Improve cross platform support

--- stringLinker.hs
+++ stringLinker.hs
@@ -9,6 +9,13 @@ import System.Process
 import Distribution.System
 import Data.Maybe
 import Data.List
+import Data.Char
+
+mapFst :: (a -> b) -> (a, c) -> (b, c)
+mapFst f (x, y) = (f x, y)
+
+mapSnd :: (a -> b) -> (c, a) -> (c, b)
+mapSnd f (x, y) = (x, f y)
 
 nativeOutputFormat :: String
 nativeOutputFormat = objF ++ bitSize
@@ -20,17 +27,29 @@ nativeOutputFormat = objF ++ bitSize
                     I386   -> "32"
                     X86_64 -> "64"
 
+register :: Arch -> String
+register I386   = "eax"
+register X86_64 = "rax"
+
+transformIdent :: String -> String
+transformIdent = case buildOS of
+                   Linux   -> id
+                   OSX     -> ("_" ++)
+                   Windows -> ("_" ++)
+
 data Options = Options
-  { optInput   :: [IO (String, String)]
-  , optOutput  :: Maybe FilePath
-  , optOutputF :: String
+  { optInput      :: [IO (String, String)]
+  , optOutput     :: Maybe FilePath
+  , optOutputF    :: String
+  , optOutputArch :: Arch
   }
 
 defaultOptions :: Options
 defaultOptions = Options
-  { optInput   = []
-  , optOutput  = Nothing
-  , optOutputF = nativeOutputFormat
+  { optInput      = []
+  , optOutput     = Nothing
+  , optOutputF    = nativeOutputFormat
+  , optOutputArch = buildArch
   }
 
 inputOpt :: String -> Options -> IO Options
@@ -59,9 +78,30 @@ options =
       "f"
       ["format"]
       (ReqArg
-        (\arg opt -> return opt { optOutputF = arg })
+        (\arg opt -> return opt {
+            optOutputF = arg,
+            optOutputArch = case reverse . take 2 . reverse $ arg of
+                              "32" -> I386
+                              "64" -> X86_64
+                              _    -> optOutputArch opt
+          })
         "FORMAT")
       "Set output format"
+  , Option
+      "a"
+      ["arch", "architecture"]
+      (ReqArg
+        (\arg opt -> return opt {
+            optOutputArch = case map toLower arg of
+                              "32"     -> I386
+                              "i386"   -> I386
+                              "x86"    -> I386
+                              "64"     -> X86_64
+                              "x64"    -> X86_64
+                              "x86_64" -> X86_64
+          })
+        "ARCH")
+      "Set output architecture"
   , Option
       "h"
       ["help"]
@@ -73,30 +113,30 @@ options =
       "Show help"
   ]
 
-compileFunction :: String -> (String, String) -> String
-compileFunction format (ident, str) = "global _"
+compileFunction :: Arch -> (String, String) -> String
+compileFunction arch (ident, str) = "global "
                                    ++ ident
-                                   ++ "\n_"
+                                   ++ "\n"
                                    ++ ident
                                    ++ ":\nlea "
-                                   ++ (if (reverse . take 2 . reverse $ format) == "64" then "rax" else "eax")
-                                   ++ ",[rel __"
+                                   ++ register arch
+                                   ++ ",[rel _"
                                    ++ ident
                                    ++ "]\nret"
 
 compileString :: (String, String) -> String
-compileString (ident, str) = "global __"
+compileString (ident, str) = "global _"
                           ++ ident
-                          ++ "\n__"
+                          ++ "\n_"
                           ++ ident
                           ++ " DB "
                           ++ (intercalate "," . map (show . fromEnum) $ str)
                           ++ ",0"
 
-compile :: String -> [(String, String)] -> String
-compile format xs = intercalate "\n" . map ($ xs) $
+compile :: Arch -> [(String, String)] -> String
+compile arch xs = intercalate "\n" . map ($ map (mapFst transformIdent) xs) $
   [ const "section .text"
-  , intercalate "\n" . map (compileFunction format $)
+  , intercalate "\n" . map (compileFunction arch $)
   , const "section .data"
   , intercalate "\n" . map (compileString $)
   ]
@@ -128,7 +168,8 @@ main = do
 
   let output = fromJust . optOutput $ opts
 
-  writeFile (output ++ ".asm") . compile (optOutputF opts) $ input
+  writeFile (output ++ ".asm") . compile (optOutputArch opts) $ input
   writeFile (output ++ ".h") . generateHeader $ input
 
   callProcess "nasm" ["-f", optOutputF opts, output ++ ".asm"]
+
